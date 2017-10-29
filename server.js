8689939406502026c9bf9c1cc5153bf51a80e8c2
root <Andrlex>
2017-10-29 23:17:27 +0000

    config ssl

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+#!/usr/bin/env nodejs
 'use strict';
 
 const Hapi = require('hapi');
 const Server = new Hapi.Server();
+const Intert = require('inert');
+const Path = require('path');
 
 const routes = require('./src/api/index.js');
 const ingest = require('./src/middleware/vtc.ingester');
 
+let tls = {
+	key: fs.readFileSync('/etc/ssl/vertcoin-dashboard.com.key'),
+	cert: fs.readFileSync('/etc/ssl/vertcoin-dashboard_com.crt')
+};
+
 Server.connection({
-	host: '192.168.0.5',
-	port: 3001
+	host: '54.37.64.232',
+	port: 443,
+	tls: tls
 });
 
 Server.start((err) =>
@@ -24,3 +33,23 @@ Server.start((err) =>
 });
 
 Server.route(routes);
+
+Server.register([
+	Intert
+], (err) => {
+	if (err)
+		throw err;
+
+	Server.route({
+		method: '/GET',
+		path: '/{path*}',
+		handler: {
+			directory: {
+				path: Path.resolve('/var/www/vertcoin-dashboard.com/public'),
+				listing: false,
+				index: true
+			}
+		}
+	})
+
+});
